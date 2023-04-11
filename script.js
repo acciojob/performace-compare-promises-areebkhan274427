292b9915aeec5a1eaf5915a532ca31d7c1eb943c
Areeb Khan <55325380+areebkhan274427@users.noreply.github.com>
2023-04-11 21:48:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -11,5 +11,27 @@ const apiUrls = [
   "https://jsonplaceholder.typicode.com/todos/9",
   "https://jsonplaceholder.typicode.com/todos/10",
 ];
+async function fetchApis() {
+        const startAll = performance.now();
+        const allResponses = await Promise.all(
+          apiUrls.map(url => fetch(url))
+        );
+        const endAll = performance.now();
 
+        const startAny = performance.now();
+        const anyResponse = await Promise.any(
+          apiUrls.map(url => fetch(url))
+        );
+        const endAny = performance.now();
+
+        const all = document.getElementById("output-all");
+	const any = document.getElementById("output-any");
+        const allTime = endAll - startAll;
+        const anyTime = endAny - startAny;
+        all.innerText = allTime;
+	any.innerText = anyTime;
+      }
+
+fetchApis();
 // You can write your code here
+
